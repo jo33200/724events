3a190eef47459d628e0988cb5ae922c4e6a38249
jo33200 <jo33200@gmail.com>
2024-05-23 15:53:19 +0200

    fix: descending order

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,7 +8,7 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
     setTimeout(
@@ -21,7 +21,7 @@ const Slider = () => {
   }, [index, byDateDesc]);
   return (
     <div className="SlideCardList">
-      {byDateDesc?.slice().reverse().map((event, idx) => (
+      {byDateDesc?.map((event, idx) => (
         <>
           <div
             key={event.id || event.title}
